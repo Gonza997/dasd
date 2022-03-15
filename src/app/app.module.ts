f28e344d8d600626b4a568d1b97c42fc1a9b007b
Gonza997 <gsyanke@gmail.com>
2022-03-14 21:06:35 -0300

    Tr_works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componente/header/header.component';
 import { AsideComponent } from './componente/aside/aside.component';
+import { WorksComponent } from './componente/works/works.component';
+import { WorkItemComponent } from './componente/work-item/work-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AsideComponent
+    AsideComponent,
+    WorksComponent,
+    WorkItemComponent
   ],
   imports: [
     BrowserModule,
